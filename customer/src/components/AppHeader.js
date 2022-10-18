69ef6a202568a04310eb4c1b3a038cf1347583a1
ubaid541 <malikunknown633@gmail.com>
2022-10-18 11:36:08 +0500

    Customer section safe point

--- customer/src/components/AppHeader.js
+++ customer/src/components/AppHeader.js
@@ -12,7 +12,7 @@ import {
   CNavItem,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-import { cilBell, cilEnvelopeOpen, cilList, cilMenu } from '@coreui/icons'
+import { cilBell, cilCart, cilEnvelopeOpen, cilList, cilMenu } from '@coreui/icons'
 
 import { AppBreadcrumb } from './index'
 import { AppHeaderDropdown } from './header/index'
@@ -23,54 +23,61 @@ const AppHeader = () => {
   const sidebarShow = useSelector((state) => state.sidebarShow)
 
   return (
-    <CHeader position="sticky" className="mb-4">
+    <CHeader position="sticky">
       <CContainer fluid>
-        <CHeaderToggler
+        {/* <CHeaderToggler
           className="ps-1"
           onClick={() => dispatch({ type: 'set', sidebarShow: !sidebarShow })}
         >
           <CIcon icon={cilMenu} size="lg" />
-        </CHeaderToggler>
+        </CHeaderToggler> */}
         <CHeaderBrand className="mx-auto d-md-none" to="/">
           <CIcon icon={logo} height={48} alt="Logo" />
         </CHeaderBrand>
         <CHeaderNav className="d-none d-md-flex me-auto">
           <CNavItem>
-            <CNavLink to="/dashboard" component={NavLink}>
-              Dashboard
-            </CNavLink>
+            <CNavLink to="/">Home</CNavLink>
           </CNavItem>
           <CNavItem>
-            <CNavLink href="#">Users</CNavLink>
+            <CNavLink href="/customer/products">Products</CNavLink>
           </CNavItem>
           <CNavItem>
-            <CNavLink href="#">Settings</CNavLink>
+            <CNavLink href="/customer/categories">Categories</CNavLink>
+          </CNavItem>
+          <CNavItem>
+            <CNavLink href="/customer/businesses">Businesses</CNavLink>
           </CNavItem>
         </CHeaderNav>
         <CHeaderNav>
+          <form className="d-flex ms-4" method="get" action="/search-result">
+            <input
+              className="form-control me-2"
+              type="text"
+              name="pro_box"
+              id="pro_box"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" id="search_product" type="submit">
+              Search
+            </button>
+            <div
+              className="pro_list list-group"
+              style={{ position: 'absolute', marginTop: ' 2.2%' }}
+              id="pro_list"
+            ></div>
+          </form>
+        </CHeaderNav>
+        <CHeaderNav>
           <CNavItem>
-            <CNavLink href="#">
-              <CIcon icon={cilBell} size="lg" />
-            </CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">
-              <CIcon icon={cilList} size="lg" />
-            </CNavLink>
-          </CNavItem>
-          <CNavItem>
-            <CNavLink href="#">
-              <CIcon icon={cilEnvelopeOpen} size="lg" />
+            <CNavLink href="/customer/cart">
+              <CIcon icon={cilCart} size="lg" />
             </CNavLink>
           </CNavItem>
         </CHeaderNav>
         <CHeaderNav className="ms-3">
           <AppHeaderDropdown />
         </CHeaderNav>
-      </CContainer>
-      <CHeaderDivider />
-      <CContainer fluid>
-        <AppBreadcrumb />
       </CContainer>
     </CHeader>
   )
--- customer/src/layout/DefaultLayout.js
+++ customer/src/layout/DefaultLayout.js
@@ -4,7 +4,6 @@ import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/inde
 const DefaultLayout = () => {
   return (
     <div>
-      <AppSidebar />
       <div className="wrapper d-flex flex-column min-vh-100 bg-light">
         <AppHeader />
         <div className="body flex-grow-1 px-3">
